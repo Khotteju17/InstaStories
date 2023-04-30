9444823a5b9d9efb9c2b9e8935beb0eadfc0720c
Khotteju17 <89903569+Khotteju17@users.noreply.github.com>
2023-04-30 21:20:58 +0530

    Created story functionality similar to Instagram stories
    Each story must consist of both photos and videos Slide to switch story
    Tap to switch items in a story
    You need to showcase the progress bar
    Progress bar depends on video duration Progress bar will pause when story is paused

---
+++ InstagramStoriesApp/InstagramStoriesApp/Controller/MainViewController.swift
@@ -0,0 +1,81 @@
+//
+//  MainViewController.swift
+//  InstagramStoriesApp
+//
+//  Created by Tejashree on 29/04/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var usersCollectionView: UICollectionView!
+    
+    var userDetails: [UserDetails] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        fetchUserData()
+        usersCollectionView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Private Func
+    private func fetchUserData() {
+        let path = Bundle.main.path(forResource: "user-details", ofType: "json")
+        let data = NSData(contentsOfFile: path ?? "") as Data?
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            if let aUserDetails = json["userDetails"] as? [[String : Any]] {
+                for element in aUserDetails {
+                    userDetails += [UserDetails(userDetails: element)]
+                }
+            }
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return userDetails.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! usersCollectionViewCell
+        cell.imgView.imageFromServerURL(userDetails[indexPath.row].imageUrl)
+        cell.lblUserName.text = userDetails[indexPath.row].name
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        DispatchQueue.main.async {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "ContentView") as! ContentViewController
+            vc.modalPresentationStyle = .overFullScreen
+            vc.pages = self.userDetails
+            vc.currentIndex = indexPath.row
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+}
+    
+
+
